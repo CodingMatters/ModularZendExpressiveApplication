5ff60244bf61ac5b77d38921dd6941fba4a6f1a7
gabbydgab <gamba@gabbydgab.com>
2016-08-20 14:50:52 +0800

    fixed zend expressive package configuration dependencies

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,7 +6,8 @@ use Zend\Expressive\ConfigManager\ConfigManager;
  * Use Fully Qualified Namespace to enable the expressive configuration
  */
 $modules = [
-    Zend\Expressive\ConfigProvider::class, //Application Settings
+    Zend\Expressive\ConfigProvider::class, // Minimal Components
+    Zend\Component\ConfigProvider::class, // Zend Components
     App\ConfigProvider::class, //Application Settings
     //Append module namespace here
 ];
