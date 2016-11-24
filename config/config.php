061ce008d40d4c3ddd7833d88a77e26afe714697
gabbydgab <gamba@gabbydgab.com>
2016-11-24 13:17:47 +0800

    fixed configuration settings

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+use Zend\Expressive\ConfigManager\ConfigManager;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Stdlib\Glob;
 
@@ -12,29 +13,32 @@ use Zend\Stdlib\Glob;
  * Obviously, if you use closures in your config you can't cache it.
  */
 
-$cachedConfigFile = 'data/cache/app_config.php';
+$cachedConfigFile = 'data/cache/application.config.php';
 
 $config = [];
 if (is_file($cachedConfigFile)) {
     // Try to load the cached config
     $config = include $cachedConfigFile;
 } else {
-    // Configuration from loaded modules (including vendor)
-    $config = ArrayUtils::merge($config, require __DIR__ . '/modules.config.php');
-    
+
+    // Configuration from loaded modules (including vendor via zend-component-installer)
+    $modules = require __DIR__ . '/modules.config.php';
+    $mergedModules = (new ConfigManager($modules))->getMergedConfig();
+    $config = ArrayUtils::merge($config, $mergedModules);
+
     // Load configuration from autoload path
     foreach (Glob::glob('config/autoload/{{,*.}global,{,*.}local}.php', Glob::GLOB_BRACE) as $file) {
         $config = ArrayUtils::merge($config, include $file);
     }
-    
-    // Development mode enabled
-    if (file_exists(__DIR__ . '/../config/development.config.php')) {
-        $config = ArrayUtils::merge($config, require __DIR__ . '/../config/development.config.php');
-    }
-    
+
     // Cache config if enabled
     if (isset($config['config_cache_enabled']) && $config['config_cache_enabled'] === true) {
         file_put_contents($cachedConfigFile, '<?php return ' . var_export($config, true) . ';');
+    }
+
+    // Development mode enabled
+    if (file_exists(__DIR__ . '/../config/development.config.php')) {
+        $config = ArrayUtils::merge($config, require __DIR__ . '/../config/development.config.php');
     }
 }
 
