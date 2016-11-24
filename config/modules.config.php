09a58ebe53a188d16c0ee79d762385db06f58f55
gabbydgab <gamba@gabbydgab.com>
2016-11-24 13:21:50 +0800

    updated

--- config/modules.config.php
+++ config/modules.config.php
@@ -1,14 +1,10 @@
 <?php
 
-use Zend\Expressive\ConfigManager\ConfigManager;
-
 /**
  * Use Fully Qualified Namespace to enable the expressive configuration
  */
-$modules = [
-    Zend\Expressive\ConfigProvider::class, // Minimal Components
-    Zend\Component\ConfigProvider::class, // Zend Components
-    App\ConfigProvider::class, //Application Settings
-    //Append module namespace here
+return [
+    Zend\Expressive\ConfigProvider::class,
+    Zend\Component\ConfigProvider::class,
+    App\ConfigProvider::class,    
 ];
-return (new ConfigManager($modules))->getMergedConfig();
