5d7e7b69fb659ff253b07270af07a99b218852de
gabbydgab <gamba@gabbydgab.com>
2016-12-09 03:02:57 +0800

    fixed order of classes

--- module/App/src/Action/HomePageAction.php
+++ module/App/src/Action/HomePageAction.php
@@ -6,9 +6,9 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
+use Zend\Expressive\Plates\PlatesRenderer;
 use Zend\Expressive\Router;
 use Zend\Expressive\Template;
-use Zend\Expressive\Plates\PlatesRenderer;
 use Zend\Expressive\Twig\TwigRenderer;
 use Zend\Expressive\ZendView\ZendViewRenderer;
 
