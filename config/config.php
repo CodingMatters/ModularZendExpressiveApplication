2d0aacab4d4b8fae729452c2733c278aa2dd14b9
gabbydgab <gamba@gabbydgab.com>
2017-01-23 11:40:26 +0800

    fixed implementation

--- config/config.php
+++ config/config.php
@@ -1,46 +1,21 @@
 <?php
 
 use Zend\ConfigAggregator\ConfigAggregator;
-use Zend\Stdlib\ArrayUtils;
-use Zend\Stdlib\Glob;
+use Zend\ConfigAggregator\PhpFileProvider;
 
-/**
- * Configuration files are loaded in a specific order. First ``global.php``, then ``*.global.php``.
- * then ``local.php`` and finally ``*.local.php``. This way local settings overwrite global settings.
- *
- * The configuration can be cached. This can be done by setting ``config_cache_enabled`` to ``true``.
- *
- * Obviously, if you use closures in your config you can't cache it.
- */
+$config = new ConfigAggregator(
+    [
+        \Zend\Router\ConfigProvider::class,
+        \Zend\Validator\ConfigProvider::class,
+        \Zend\Navigation\ConfigProvider::class,
+        \Zend\Expressive\ConfigProvider::class,
+        \Zend\Component\ConfigProvider::class,
+        \CodingMatters\ExpressiveWebHelper\ConfigProvider::class,
+        \CodingMatters\ExpressiveErrorHandler\ConfigProvider::class,
+        new PhpFileProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
+        new PhpFileProvider('config/development.config.php') // only override if development mode is ENABLED
+    ],
+    __DIR__ . '/../data/cache/application.config.php'
+);
 
-$cachedConfigFile = 'data/cache/application.config.php';
-
-$config = [];
-if (is_file($cachedConfigFile)) {
-    // Try to load the cached config
-    $config = include $cachedConfigFile;
-} else {
-    // Configuration from loaded modules (including vendor via zend-component-installer)
-    $modules = require __DIR__ . '/modules.config.php';
-    $mergedModules = (new ConfigAggregator($modules))->getMergedConfig();
-    $config = ArrayUtils::merge($config, $mergedModules);
-
-    // Load configuration from autoload path
-    foreach (Glob::glob('config/autoload/{{,*.}global,{,*.}local}.php', Glob::GLOB_BRACE) as $file) {
-        $config = ArrayUtils::merge($config, include $file);
-    }
-    
-    // Cache config if enabled
-    if (isset($config['config_cache_enabled']) && $config['config_cache_enabled'] == true) {
-        file_put_contents($cachedConfigFile, '<?php return ' . var_export($config, true) . ';');
-    }
-
-    // Development mode enabled
-    if (file_exists(__DIR__ . '/../config/development.config.php')) {
-        $config = ArrayUtils::merge($config, require __DIR__ . '/../config/development.config.php');
-    }
-}
-
-// Return an ArrayObject so we can inject the config as a service in Aura.Di
-// and still use array checks like ``is_array``.
-return new ArrayObject($config, ArrayObject::ARRAY_AS_PROPS);
+return $config->getMergedConfig();
